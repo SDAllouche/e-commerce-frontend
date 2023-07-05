8dd15efcae2a36285461394f2c3fe78b2ae0a5c8
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-07-05 10:56:21 +0100

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
